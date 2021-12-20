fa309a80d5400cf3f62934f85b651647b470b2b8
Lioncat2002 <sankhayan2002@gmail.com>
2021-12-20 21:59:00 +0530

    Meow

---
+++ src/App.svelte
@@ -0,0 +1,25 @@
+<script>
+	let url=''
+	let fullurl=`https://qrtag.net/api/qr_4.png?url=https://www.qrtag.net`
+	let toggle=false
+	function submit() {
+		fullurl=`https://qrtag.net/api/qr_4.png?url=${url}`
+		toggle=true
+	}
+</script>
+
+
+<input type="text" bind:value={url} placeholder="Enter url">
+
+<button on:click={submit}>Submit</button>
+<br>
+<style>
+	img{
+		display: grid;
+		justify-content: center;
+	}
+</style>
+{#if toggle}
+<img src={fullurl} alt="qrtag">
+{/if}
+
